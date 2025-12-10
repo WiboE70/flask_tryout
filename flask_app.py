d20bf60a615b6dccf387582a9a49cb156ac989dd
Wibo Esselink <wibo.esselink@ziggo.nl>
2025-12-10 21:34:09 +0100

    tekst aangepast

--- flask_app.py
+++ flask_app.py
@@ -2,6 +2,6 @@ from flask import Flask
 app=Flask(_name_)
 @app.route('/')
 def home():
-    return "<p>Hello world!</p>"
+    return "<p>De veranderende tekst</p>"
 if __name__ == '__main__':
        app.run(port=5000,debug=True)
